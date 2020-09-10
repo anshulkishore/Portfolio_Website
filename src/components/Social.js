ba5213049ea26c55a02af82f95963de9b81f8a8a
Anshul Kishore <anshulkishore310@gmail.com>
2020-09-10 20:48:06 +0530

    added noopener noreferrer

--- src/components/Social.js
+++ src/components/Social.js
@@ -7,19 +7,19 @@ class Social extends Component {
                                 
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css"></link>
                 
-                <a href="https://github.com/anshulkishore" target="_blank">
+                <a href="https://github.com/anshulkishore" target="_blank" rel="noopener noreferrer">
                 <i class="fab fa-github"></i>
                 </a>
                 
-                <a href="https://www.instagram.com/anshul_kishore" target="_blank">
+                <a href="https://www.instagram.com/anshul_kishore" target="_blank" rel="noopener noreferrer">
                 <i class="fab fa-instagram"></i>
                 </a>
                 
-                <a href="https://www.facebook.com/anshul.kishore" target="_blank">
+                <a href="https://www.facebook.com/anshul.kishore" target="_blank" rel="noopener noreferrer">
                 <i class="fab fa-facebook-f"></i>
                 </a>
                 
-                <a href="https://www.linkedin.com/in/anshulkishore03/" target="_blank">
+                <a href="https://www.linkedin.com/in/anshulkishore03/" target="_blank" rel="noopener noreferrer">
                 <i class="fab fa-linkedin-in"></i>
                 </a>
             </div>
