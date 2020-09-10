80dd0e2f489d23a529ae5c10e0c8934ea57e9473
Anshul Kishore <anshulkishore310@gmail.com>
2020-09-10 20:43:18 +0530

    added alt prop in img

--- src/contents/Home.js
+++ src/contents/Home.js
@@ -7,7 +7,7 @@ class Home extends Component {
     render() {
         return (
             <div className="condiv home">
-                <img src={profilepic} className="profilepic" ></img>
+                <img src={profilepic} className="profilepic" alt="profile pic"></img>
                 <ReactTypingEffect className="typingeffect" text={['I am Anshul Kishore','I am a software engineer']} speed={100} eraseDelay={700}/>
                 <Social />
             </div>
